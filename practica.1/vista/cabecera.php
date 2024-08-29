fafdeb7aa0d4424354c1d7130821f5cc0f61d169
kpazamarro <126477045+kpazamarro@users.noreply.github.com>
2024-08-29 00:22:19 -0400

    Add files via upload

--- practica.1/vista/cabecera.php
+++ practica.1/vista/cabecera.php
@@ -10,23 +10,20 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">JARDINERO</a>
+            <a class="navbar-brand" href="#">Pagina Web Principal</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                <li class="nav-item">
-                        <a class="nav-link" href="registroEmpleado.php">Registrar Empleado</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="registrocliente.php">Registrar Cliente</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="registroOficina.php">Registrar Oficina</a>
+                        <a class="nav-link" href="registrooficina.php">Registrar Oficina</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="registroPedido.php">Registrar Pedido</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="registroCliente.php">Registrar Cliente</a>
+                        <a class="nav-link" href="registropedido.php">Registrar Pedido</a>
                     </li>
                 </ul>
             </div>
